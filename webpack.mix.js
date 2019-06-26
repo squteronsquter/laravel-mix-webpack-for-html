b4b61b68e9d9fae7ca7d83f8275fdc25a48b424e
squteronsquter <squter@squter.pl>
2019-06-26 10:45:04 +0200

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require('laravel-mix');
+mix
+  .js('resources/scripts/app.js', 'public/js/app.js')
+  .sass('resources/styles/app.scss', 'public/css/app.css')
+  .copyDirectory('src', 'public');
